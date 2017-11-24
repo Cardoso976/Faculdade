d51ae9d2d82f1d03fd4ca1fddc4fb9e60b444706
Rodrigo Garcia Cardoso <rodrigo_cardoso97@hotmail.com>
2017-11-24 14:55:30 -0200

    Banco de Dados

--- Banco de Dados/Trabalhos N2/E5-Transporte Marítimo/E5-Transporte Marítimo.sql
+++ Banco de Dados/Trabalhos N2/E5-Transporte Marítimo/E5-Transporte Marítimo.sql
@@ -14,8 +14,10 @@ CREATE TABLE Porto(
 CREATE TABLE Rota(
 	Navio_Nome varchar(45),
 	Porto_Nome varchar(45),
-	CONSTRAINT fk_Rota_Navio FOREIGN KEY (Navio_Nome) REFERENCES Navio(Nome),
-	CONSTRAINT fk_Rota_Porto FOREIGN KEY (Porto_Nome) REFERENCES Porto(Nome)
+	Data_Maxima_Desenbarque date,
+	CONSTRAINT PRIMARY KEY (Navio_Nome, Porto_Nome),
+	CONSTRAINT fk_Navio_Nome FOREIGN KEY (Navio_Nome) REFERENCES Navio(nome),
+	CONSTRAINT fk_Porto_Nome FOREIGN KEY (Porto_Nome) REFERENCES Porto(nome)
 );
 
 CREATE TABLE Agente(
@@ -23,42 +25,31 @@ CREATE TABLE Agente(
 	nome varchar(45),
 	Porto_Nome varchar(45),
 	CONSTRAINT PRIMARY KEY (codigo),
-	CONSTRAINT fk_Agente_Porto FOREIGN KEY (Porto_Nome) REFERENCES Porto(Nome)
+	CONSTRAINT fk_Porto_N FOREIGN KEY (Porto_Nome) REFERENCES Porto(Nome)
 );
 
 CREATE TABLE Carga(
 	numero int NOT NULL,
 	peso decimal(9,2),
-	Agente_codigo int,
-	Agente_Porto_Nome varchar(45),
+	Agente_codigo int,	
 	Navio_Nome varchar(45),
+	Porto_Nome varchar(45),
 	CONSTRAINT PRIMARY KEY (numero),
-	CONSTRAINT fk_Carga_Agente FOREIGN KEY (Agente_codigo, Agente_Porto_Nome) REFERENCES Agente (codigo, nome),
-	CONSTRAINT fk_Carga_Navio FOREIGN KEY (Navio_Nome) REFERENCES Navio(Nome)
+	CONSTRAINT fk_Carga_Agente FOREIGN KEY (Agente_codigo) REFERENCES Agente (codigo),
+	CONSTRAINT fk_Carga_Navio FOREIGN KEY (Navio_Nome) REFERENCES Navio(Nome),
+	CONSTRAINT fk_Carga_Porto FOREIGN KEY (Porto_Nome) REFERENCES Porto(Nome)
 );
 
 CREATE TABLE Sensivel(
 	Carga_numero int,
-	temperatura_maxima decimal(9,2),	
-	CONSTRAINT fk_Sensivel_Carga PRIMARY KEY (Carga_numero) REFERENCES Carga(numero)
+	temperatura_maxima decimal(9,2),
+	CONSTRAINT PRIMARY KEY (Carga_numero),	
+	CONSTRAINT fk_Sensivel_Carga FOREIGN KEY (Carga_numero) REFERENCES Carga(numero)
 );
 
 CREATE TABLE Perecivel(
 	Carga_numero int,
 	data_validade date,	
-	CONSTRAINT fk_Sensivel_Carga PRIMARY KEY (Carga_numero) REFERENCES Carga(numero)
-);
-
-CREATE TABLE Destino(
-	Porto_Nome varchar(45),
-	Carga_numero int,
-	Carga_Agente_Codigo int,
-	Carga_Agente_Porto_Nome varchar(45),
-	Carga_Navio_Nome varchar(45),
-	data_maxima_desembarque date,
-	CONSTRAINT PRIMARY KEY (Porto_Nome, Carga_numero, Carga_Agente_Codigo, Carga_Agente_Porto_Nome, Carga_Navio_Nome),
-	CONSTRAINT fk_Destino_Carga FOREIGN KEY (Carga_numero, Carga_Agente_Codigo, Carga_Agente_Porto_Nome, Carga_Navio_Nome) 
-	REFERENCES Carga (numero, Agente_codigo, Agente_Porto_Nome, Navio_Nome),
-	CONSTRAINT fk_Destino_Porto FOREIGN KEY (Porto_Nome) 
-	REFERENCES Porto(Nome)
+	CONSTRAINT PRIMARY KEY (Carga_numero),
+	CONSTRAINT fk_Perecivel_Carga FOREIGN KEY (Carga_numero) REFERENCES Carga(numero)
 );
